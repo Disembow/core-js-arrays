9efc359435ad08452778156e41b5fe387e225577
Disembow <e_naletov@mail.ru>
2024-01-17 13:58:24 +0100

    fix: update repo, fix issues

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -51,7 +51,7 @@ function generateOdds(len) {
  *    [] => []
  */
 function doubleArray(arr) {
-  return [...arr, ...arr];
+  return arr.concat(arr);
 }
 
 /**
@@ -170,6 +170,8 @@ function getHead(arr, n) {
  *    [ 'a', 'b', 'c', 'd'], 3  => [ 'b', 'c', 'd' ]
  */
 function getTail(arr, n) {
+  if (n === 0) return [];
+
   return arr.slice(-n);
 }
 
@@ -455,6 +457,24 @@ function getIdentityMatrix(/* n */) {
 }
 
 /**
+ * Returns the average of all items in the specified array of numbers.
+ * The result should be rounded to two decimal places.
+ *
+ * @param {array} arr - The input array
+ * @return {number} - The average of all items
+ *
+ * @example
+ *   getAverage([]) => 0
+ *   getAverage([ 1, 2, 3 ]) => 2
+ *   getAverage([ -1, 1, -1, 1 ]) => 0
+ *   getAverage([ 1, 10, 100, 1000 ])  => 277,75
+ *   getAverage([ 2, 3, 3 ])  => 2,67
+ */
+function getAverage(/* arr */) {
+  throw new Error('Not implemented');
+}
+
+/**
  * Creates an array of integers from the specified start to end (inclusive)
  *
  * @param {number} start
@@ -521,6 +541,23 @@ function group(/* array, keySelector, valueSelector */) {
 }
 
 /**
+ * Returns a new array where each element is the sum of the corresponding elements
+ * from two arrays. Arrays can have different lengths.
+ *
+ * @param {array} arr1 - The first array.
+ * @param {array} arr2 - The second array.
+ * @return {array} - An array containing the sum of corresponding elements.
+ *
+ * @example
+ *    sumArrays([1, 2, 3], [4, 5, 6]) => [5, 7, 9]
+ *    sumArrays([10, 20, 30], [5, 10, 15]) => [15, 30, 45]
+ *    sumArrays([-1, 0, 1], [1, 2, 3, 4]) => [0, 2, 4, 4]
+ */
+function sumArrays(/* arr1, arr2 */) {
+  throw new Error('Not implemented');
+}
+
+/**
  * Projects each element of the specified array to a sequence
  * and flattens the resulting sequences into one array.
  *
@@ -575,6 +612,211 @@ function swapHeadAndTail(/* arr */) {
   throw new Error('Not implemented');
 }
 
+/**
+ * Checks if all strings in an array have the same length.
+ *
+ * @param {array} arr - The array of strings to be checked.
+ * @return {boolean} - True if all strings have the same length, false otherwise.
+ *
+ * @example
+ *    isSameLength(['orange', 'banana', 'cherry']) => true
+ *    isSameLength(['cat', 'dog', 'elephant']) => false
+ */
+function isSameLength(/* arr */) {
+  throw new Error('Not implemented');
+}
+
+/**
+ * Checks if there are elements in the array where the value is equal to its index.
+ *
+ * @param {array} arr - The array of elements to be checked.
+ * @return {boolean} - True if there are elements with value equal to their index, false otherwise.
+ *
+ * @example
+ *    isValueEqualsIndex([0, 1, 2, 3, 4]) => true
+ *    isValueEqualsIndex([2, 1, 0, 4, 5]) => true
+ *    isValueEqualsIndex([10, 20, 30, 40, 50]) => false
+ */
+function isValueEqualsIndex(/* arr */) {
+  throw new Error('Not implemented');
+}
+
+/**
+ * Creates an n-dimensional array and fills it with zeros.
+ *
+ * @param {number} n - Depth of outter array (n > 0).
+ * @param {number} size - Length of all arrays (size > 0).
+ * @return {array} - The n-dimensional array filled with zeros.
+ *
+ * @example
+ *    createNDimensionalArray(2, 3) => [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+ *    createNDimensionalArray(3, 2) => [[[0, 0], [0, 0]], [[0, 0], [0, 0]]]
+ *    createNDimensionalArray(4, 2) => [[[[0, 0], [0, 0]], [[0, 0], [0, 0]]], [[[0, 0], [0, 0]], [[0, 0], [0, 0]]]]
+ *    createNDimensionalArray(1, 1) => [0]
+ */
+function createNDimensionalArray(/* n, size */) {
+  throw new Error('Not implemented');
+}
+
+/**
+ * Flattens a nested array into a single-level array.
+ *
+ * @param {array} nestedArray - The nested array to be flattened.
+ * @return {array} - A single-level array.
+ *
+ * @example
+ *    flattenArray([1, [2, [3, 4], 5], 6]) => [1, 2, 3, 4, 5, 6]
+ *    flattenArray(['a', ['b', ['c', 'd'], 'e'], 'f']) => ['a', 'b', 'c', 'd', 'e', 'f']
+ *    flattenArray([1, 2, 3, 4]) => [1, 2, 3, 4]
+ */
+function flattenArray(/* nestedArray */) {
+  throw new Error('Not implemented');
+}
+
+/**
+ * Every month, you record your income and expenses.
+ * Expenses may be greater than income.
+ * You need to calculate the final balance.
+ *
+ * @param {array} arr - The input array [[income, expence], ...]
+ * @return {number} - The final balance.
+ *
+ * @example
+ *   calculateBalance([ [ 10, 8 ], [ 5, 1 ] ]) => (10 - 8) + (5 - 1) = 2 + 4 = 6
+ *   calculateBalance([ [ 10, 8 ], [ 1, 5 ] ])  => (10 - 8) + (1 - 5) = 2 + -4 = -2
+ *   calculateBalance([]) => 0
+ */
+function calculateBalance(/* arr */) {
+  throw new Error('Not implemented');
+}
+
+/**
+ * Breaks an array into chunks of the specified size.
+ *
+ * @param {array} arr - The array to be broken into chunks.
+ * @param {number} chunkSize - The size of each chunk.
+ * @return {array} - An array of chunks.
+ *
+ * @example
+ *    createChunks([1, 2, 3, 4, 5, 6, 7], 3) => [[1, 2, 3], [4, 5, 6], [7]]
+ *    createChunks(['a', 'b', 'c', 'd', 'e'], 2) => [['a', 'b'], ['c', 'd'], ['e']]
+ *    createChunks([10, 20, 30, 40, 50], 1) => [[10], [20], [30], [40], [50]]
+ */
+function createChunks(/* arr, chunkSize */) {
+  throw new Error('Not implemented');
+}
+
+/**
+ * Returns an element from the multidimensional array by the specified indices.
+ *
+ * @param {array} arr - The input multidimensional array
+ * @param {array} indices - The array of indices
+ * @return {any} - An element from the array
+ *
+ * @example
+ *   getElementByIndices([[1, 2], [3, 4], [5, 6]], [0,0]) => 1        (arr[0][0])
+ *   getElementByIndices(['one','two','three'], [2]) => 'three'  (arr[2])
+ *   getElementByIndices([[[ 1, 2, 3]]], [ 0, 0, 1 ]) => 2        (arr[0][0][1])
+ */
+function getElementByIndices(/* arr, indices */) {
+  throw new Error('Not implemented');
+}
+
+/**
+ * Returns an array containing indices of odd elements in the input array.
+ *
+ * @param {array} numbers - The array of numbers.
+ * @return {array} - An array containing indices of odd elements.
+ *
+ * @example
+ *    getIndicesOfOddNumbers([1, 2, 3, 4, 5]) => [0, 2, 4]
+ *    getIndicesOfOddNumbers([2, 4, 6, 8, 10]) => []
+ *    getIndicesOfOddNumbers([11, 22, 33, 44, 55]) => [0, 2, 4]
+ */
+function getIndicesOfOddNumbers(/* numbers */) {
+  throw new Error('Not implemented');
+}
+
+/**
+ * Returns the array of RGB Hex strings from the specified array of numbers.
+ *
+ * @param {array} arr - The input array.
+ * @return {array} - The array of RGB Hex strings.
+ *
+ * @example
+ *    getHexRGBValues([ 0, 255, 16777215]) => [ '#000000', '#0000FF', '#FFFFFF' ]
+ *    getHexRGBValues([]) => []
+ */
+function getHexRGBValues(/* arr */) {
+  throw new Error('Not implemented');
+}
+
+/**
+ * Returns the n largest values from the specified array
+ *
+ * @param {array} arr - The input array
+ * @param {number} n - Number of maximum values.
+ * @return {array} - n largest values.
+ *
+ * @example
+ *   getMaxItems([], 5) => []
+ *   getMaxItems([ 1, 2 ], 1) => [ 2]
+ *   getMaxItems([ 2, 3, 1 ], 2) => [ 3, 2]
+ *   getMaxItems([ 10, 2, 7, 5, 3, -5 ], 3) => [ 10, 7, 5 ]
+ *   getMaxItems([ 10, 10, 10, 10 ], 3) => [ 10, 10, 10 ]
+ */
+function getMaxItems(/* arr, n */) {
+  throw new Error('Not implemented');
+}
+
+/**
+ * Finds and returns an array containing only the common elements found in two arrays.
+ *
+ * @param {array} arr1 - The first array.
+ * @param {array} arr2 - The second array.
+ * @return {array} - An array containing common elements.
+ *
+ * @example
+ *    findCommonElements([1, 2, 3], [2, 3, 4]) => [ 2, 3 ]
+ *    findCommonElements(['a', 'b', 'c'], ['b', 'c', 'd']) => [ 'b', 'c' ]
+ *    findCommonElements([1, 2, 3], ['a', 'b', 'c']) => []
+ */
+function findCommonElements(/* arr1, arr2 */) {
+  throw new Error('Not implemented');
+}
+
+/**
+ * Finds the length of the longest increasing subsequence of a given array of integers.
+ *
+ * @param {array} nums - The array of integers.
+ * @return {number} - The length of the longest increasing subsequence.
+ *
+ * @example
+ *    findLongestIncreasingSubsequence([10, 22, 9, 33, 21, 50, 41, 60, 80]) => 3
+ *    findLongestIncreasingSubsequence([3, 10, 2, 1, 20]) => 2
+ *    findLongestIncreasingSubsequence([50, 3, 10, 7, 40, 80]) => 3
+ */
+function findLongestIncreasingSubsequence(/* nums */) {
+  throw new Error('Not implemented');
+}
+
+/**
+ * Shifts an array by n positions. If n is negative, the array is shifted to the left;
+ * if positive, it is shifted to the right.
+ *
+ * @param {array} arr - The array to be shifted.
+ * @param {number} n - The number of positions to shift the array elements.
+ * @return {array} - The shifted array.
+ *
+ * @example
+ *    shiftArray([1, 2, 3, 4, 5], 2) => [4, 5, 1, 2, 3]
+ *    shiftArray(['a', 'b', 'c', 'd'], -1) => ['b', 'c', 'd', 'a']
+ *    shiftArray([10, 20, 30, 40, 50], -3) => [40, 50, 10, 20, 30]
+ */
+function shiftArray(/* arr, n */) {
+  throw new Error('Not implemented');
+}
+
 module.exports = {
   findElement,
   generateOdds,
@@ -585,6 +827,7 @@ module.exports = {
   getUpperCaseStrings,
   getStringsLength,
   insertItem,
+  getAverage,
   getHead,
   getTail,
   toCsvText,
@@ -607,4 +850,18 @@ module.exports = {
   selectMany,
   getElementByIndexes,
   swapHeadAndTail,
+  sumArrays,
+  isSameLength,
+  isValueEqualsIndex,
+  createNDimensionalArray,
+  flattenArray,
+  calculateBalance,
+  createChunks,
+  getElementByIndices,
+  getIndicesOfOddNumbers,
+  getHexRGBValues,
+  getMaxItems,
+  findCommonElements,
+  findLongestIncreasingSubsequence,
+  shiftArray,
 };
